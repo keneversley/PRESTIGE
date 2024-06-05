60a942c0e64bdb97f92d55241eb12faa206bbb4b
keneversley <krazorfbx@gmail.com>
2024-06-05 08:20:19 -0700

    Prestige

---
+++ src/Components/Assets/all_product.js
@@ -0,0 +1,552 @@
+import { Blockchain_bathroom } from "./Blockchain bathroom.jpg";
+import { Blockchain_bathroom2 } from "./Blockchain bathroom2.jpg";
+import { Blockchain_bathroom3 } from "./Blockchain bathroom3.jpg";
+import { Blockchain_bathroom4 } from "./Blockchain bathroom4.jpg";
+import { Blockchain_bathroom5 } from "./Blockchain bathroom5.jpg";
+import { Blockchain_bathroom6 } from "./Blockchain bathroom6.jpg";
+import { Blockchain_bedroom } from "./Blockchain bedroom.jpg";
+import { Blockchain_bedroom2 } from "./Blockchain bedroom2.jpg";
+import { Blockchain_bedroom3 } from "./Blockchain bedroom3.jpg";
+import { Blockchain_bedroom4 } from "./Blockchain bedroom4.jpg";
+import { Blockchain_bedroom5 } from "./Blockchain bedroom5.jpg";
+import { Blockchain_bedroom6 } from "./Blockchain bedroom6.jpg";
+import { Blockchain_bedroom7 } from "./Blockchain bedroom7.jpg";
+import { Blockchain_bedroom8 } from "./Blockchain bedroom8.jpg";
+import { Blockchain_bedroom9 } from "./Blockchain bedroom9.jpg";
+import { Blockchain_bedroom10 } from "./Blockchain bedroom10.jpg";
+import { Blockchain_bedroom11 } from "./Blockchain bedroom11.jpg";
+import { Blockchain_bedroom12 }  from "./Blockchain bedroom12.jpg";
+import { Blockchain_bedroom13 } from "./Blockchain bedroom13.jpg";
+import { Blockchain_bedroom14 } from "./Blockchain bedroom14.jpg";
+import { Blockchain_bedroom15 } from "./Blockchain bathrooms15.jpg";
+import { Blockchain_den1 } from "./Blockchain den1.jpg";
+import { Blockchain_den2 } from "./Blockchain den2.jpg";
+import { Blockchain_dinningroom1 } from "./Blockchain dinningroom1.jpg";
+import { Blockchain_dinningroom2 } from "./Blockchain dinningroom2.jpg";
+import { Blockchain_Driveway } from "./Blockchain Driveway.jpg";
+import { Blockchain_driveway2 } from "./Blockchain driveway2.jpg";
+import { Blockchain_driveway3 } from "./Blockchain driveway3.jpg";
+import { Blockchain_eating } from "./Blockchain eating.jpg";
+import { Blockchain_gameroom } from "./Blockchain gameroom.jpg";
+import { Blockchain_gameroom2 } from "./Blockchain gameroom2.jpg";
+import { Blockchain_gameroom3 } from "./Blockchain gameroom3.jpg";
+import { Blockchain_hallway } from "./Blockchain hallway.jpg";
+import { Blockchain_kitchen1 } from "./Blockchain kitchen1.jpg";
+import { Blockchain_kitchen2 } from "./Blockchain kitchen2.jpg";
+import { Blockchain_kitchen3 } from "./Blockchain kitchen3.jpg";
+import { Blockchain_kitchen4 } from "./Blockchain kitchen4.jpg";
+import { Blockchain_kitchen5 } from "./Blockchain kitchen5.jpg";
+import { Blockchain_kitchen6 } from "./Blockchain kitchen6.jpg";
+import { Blockchain_livingroom1 } from "./Blockchain livingroom1.jpg";
+import { Blockchain_livingroom2 } from "./Blockchain livingroom2.jpg";
+import { Blockchain_movietheater } from "./Blockchain movietheater.jpg";
+import { Blockchain_Movietheater2 } from "./Blockchain Movietheater2.jpg";
+import { Blockchain_Movietheater3 } from "./Blockchain Movietheater.jpg";
+import { Blockchain_outside1 } from "./Blockchain outside1.jpg";
+import { Blockchain_outside2 } from "./Blockchain outside2.jpg";
+import { Blockchain_outside3 } from "./Blockchain outside3.jpg";
+import { Blockchain_outside4 } from "./Blockchain outside4.jpg";
+import { Blockchain_outside5 } from "./Blockchain outside5.jpg";
+import { Blockchain_outside6 } from "./Blockchain outside6.jpg";
+import { Blockchain_outside7 } from "./Blockchain outside7.jpg";
+import { Blockchain_outside8 } from "./Blockchain outside8.jpg";
+import { Blockchain_outside9 } from "./Blockchain outside9.jpg";
+import { Blockchain_outside10 } from "./Blockchain outside10.jpg";
+import { Blockchain_outside11 } from "./Blockchain outside11.jpg";
+import { Blockchain_outside12 } from "./Blockchain outside12.jpg";
+import { Blockchain_outside13 } from "./Blockchain outside13.jpg";
+import { Blockchain_outside14 } from "./Blockchain outside14.jpg";
+import { Blockchain_outside15 } from "./Blockchain outside15.jpg";
+import { Blockchain_outside20 } from "./Blockchain outside20.jpg";
+import { Blockchain_outside21 } from "./Blockchain outside21.jpg";
+import { Blockchain_outside22 } from "./Blockchain outside22.jpg";
+import { Blockchain_outside23 } from "./Blockchain outside23.jpg";
+import { Blockchain_outside24 } from "./Blockchain outside24.jpg";
+import { Blockchain_outside30 } from "./Blockchain outside30.jpg";
+import { Blockchain_outside31 } from "./Blockchain outside31.jpg";
+import { Blockchain_outside33 } from "./Blockchain outside33.jpg";
+import { Blockchain_outside34 } from "./Blockchain outside34.jpg";
+import { Blockchain_outside37 } from "./Blockchain outside37.jpg";
+import { Blockchain_outside40 } from "./Blockchain outside40.jpg";
+import { Blockchain_outside50 } from "./Blockchain outside50.jpg";
+import { Blockchain_outside51 } from "./Blockchain outside51.jpg";
+import { Blockchain_outside58 } from "./Blockchain outside58.jpg";
+import { Blockchain_pool } from "./Blockchain pool.jpeg";
+import { Blockchain_shower1 } from "./Blockchain shower1.jpg";
+import { Blockchain_sink1 } from "./Blockchain sink1.jpg";
+import { Blockchain_stairs1 } from "./Blockchain stairs1.jpg";
+
+let all_product = [
+{
+    id: 1,
+    name: "Guest Bathroom with Sink and Shower",
+    category: "blockchain",
+    image: Blockchain_bathroom,
+},
+{
+    id: 2,
+    name: "Guest Bathroom with Two Sinks and Shower",
+    category: "blockchain",
+    image: Blockchain_bathroom2,
+},
+{
+    id: 3,
+    name: "Guest Bathroom with Two Sinks and Shower side view",
+    category: "blockchain",
+    image: Blockchain_bathroom3,
+},
+{
+    id: 4,
+    name: "Guest Bathroom with long sink and open shower",
+    category: "blockchain",
+    image: Blockchain_bathroom4,
+},
+{
+    id: 5,
+    name: "Guest Bathroom with long sink and open shower side view",
+    category: "blockchain",
+    image: Blockchain_bathroom5,
+},
+{
+    id: 6,
+    name: "Guest Bathroom with single Sink and Shower",
+    category: "blockchain",
+    image: Blockchain_bathroom6,
+},
+{
+    id: 7,
+    name: "Silver Bedroom",
+    category: "blockchain",
+    image: Blockchain_bedroom,
+},
+{
+    id: 8,
+    name: "Silver Bedroom side view",
+    category: "blockchain",
+    image: Blockchain_bedroom2 ,
+},
+{
+    id: 9,
+    name: "Silver Bedroom front view",
+    category: "blockchain",
+    image: Blockchain_bedroom3,
+},
+{
+    id: 10,
+    name: "Bedroom with leather couch",
+    category: "blockchain",
+    image: Blockchain_bedroom4,
+},
+{
+    id: 11,
+    name: "Bedroom with leather couch front view",
+    category: "blockchain",
+    image: Blockchain_bedroom5,
+},
+{
+    id: 12,
+    name: "Bedroom with leather couch back view",
+    category: "blockchain",
+    image: Blockchain_bedroom6,
+},
+{
+    id: 13,
+    name: "Bedroom with leather couch angle view",
+    category: "blockchain",
+    image: Blockchain_bedroom7,
+},
+{
+    id: 14,
+    name: "Bedroom with Quenn sized bed",
+    category: "blockchain",
+    image: Blockchain_bedroom8,
+},
+{
+    id: 15,
+    name: "Bedroom with Quenn sized bed front view",
+    category: "blockchain",
+    image: Blockchain_bedroom9,
+},
+{
+    id: 16,
+    name: "Bedroom with Quenn sized bed angle view",
+    category: "blockchain",
+    image: Blockchain_bedroom10,
+},
+{
+    id: 17,
+    name: "Bedroom with twin sized bed and couch",
+    category: "blockchain",
+    image: Blockchain_bedroom11,
+},
+{
+    id: 18,
+    name: "Bedroom with twin sized bed and couch side view",
+    category: "blockchain",
+    image: Blockchain_bedroom12,
+},
+{
+    id: 19,
+    name: "Bedroom with twin sized bed with wall paper",
+    category: "blockchain",
+    image: Blockchain_bedroom13,
+},
+{
+    id: 20,
+    name: "Bedroom with twin sized bed with wall paper side view",
+    category: "blockchain",
+    image: Blockchain_bedroom14,
+},
+{
+    id: 21,
+    name: "Bedroom with twin sized bed with wall paper front view",
+    category: "blockchain",
+    image: Blockchain_bedroom15,
+},
+{
+    id: 22,
+    name: "Family room with open space",
+    category: "blockchain",
+    image: Blockchain_den1,
+},
+{
+    id: 23,
+    name: "Family room with open space side view",
+    category: "blockchain",
+    image: Blockchain_den2,
+},
+{
+    id: 24,
+    name: "Family room with kitchen view",
+    category: "blockchain",
+    image: Blockchain_dinningroom1,
+},
+{
+    id: 25,
+    name: "Family room with only kitchen view",
+    category: "blockchain",
+    image: Blockchain_dinningroom2,
+},
+{
+    id: 26,
+    name: "Driveway with fireplace view",
+    category: "blockchain",
+    image: Blockchain_Driveway,
+},
+{
+    id: 27,
+    name: "Driveway with stairs view",
+    category: "blockchain",
+    image: Blockchain_driveway2,
+},
+{
+    id: 28,
+    name: "Driveway with front house view",
+    category: "blockchain",
+    image: Blockchain_driveway3,
+},
+{
+    id: 29,
+    name: "Table with family room view",
+    category: "blockchain",
+    image: Blockchain_eating,
+},
+{
+    id: 30,
+    name: "Game room with soccer table view",
+    category: "blockchain",
+    image: Blockchain_gameroom,
+},
+{
+    id: 31,
+    name: "Game room with pool table and soccer table view",
+    category: "blockchain",
+    image: Blockchain_gameroom2,
+},
+{
+    id: 32,
+    name: "Game room with pool table view",
+    category: "blockchain",
+    image: Blockchain_gameroom3,
+},
+{
+    id: 33,
+    name: "Hallway view",
+    category: "blockchain",
+    image: Blockchain_hallway,
+},
+{
+    id: 34,
+    name: "Bar view",
+    category: "blockchain",
+    image: Blockchain_kitchen1,
+},
+{
+    id: 35,
+    name: "Bar back view",
+    category: "blockchain",
+    image: Blockchain_kitchen2,
+},
+{
+    id: 36,
+    name: "Kitchen with sink view",
+    category: "blockchain",
+    image: Blockchain_kitchen3,
+},
+{
+    id: 37,
+    name: "Kitchen with bar view",
+    category: "blockchain",
+    image: Blockchain_kitchen4,
+},
+{
+    id: 38,
+    name: "Kitchen with oven view",
+    category: "blockchain",
+    image: Blockchain_kitchen5,
+},
+{
+    id: 39,
+    name: "Kitchen with angle view",
+    category: "blockchain",
+    image: Blockchain_kitchen6,
+},
+{
+    id: 40,
+    name: "Living room with bar",
+    category: "blockchain",
+    image: Blockchain_livingroom1,
+},
+{
+    id: 41,
+    name: "Living room with bar side view",
+    category: "blockchain",
+    image: Blockchain_livingroom2,
+},
+{
+    id: 42,
+    name: "Movie theatre back view ",
+    category: "blockchain",
+    image: Blockchain_movietheater,
+},
+{
+    id: 43,
+    name: "Movie theatre side view ",
+    category: "blockchain",
+    image: Blockchain_Movietheater2,
+},
+{
+    id: 44,
+    name: "Movie theatre front view ",
+    category: "blockchain",
+    image: Blockchain_Movietheater3,
+},
+{
+    id: 45,
+    name: "Movie theatre back view ",
+    category: "blockchain",
+    image: Blockchain_Movietheater2,
+},
+{
+    id: 46,
+    name: "Outside grill view",
+    category: "blockchain",
+    image: Blockchain_outside1,
+},
+{
+    id: 47,
+    name: "Couch pool view ",
+    category: "blockchain",
+    image: Blockchain_outside2,
+},
+{
+    id: 48,
+    name: "Pool couch angle view ",
+    category: "blockchain",
+    image: Blockchain_outside3,
+},
+{
+    id: 49,
+    name: "Pool back view ",
+    category: "blockchain",
+    image: Blockchain_outside4,
+},
+{
+    id: 50,
+    name: "Pool back view with statue",
+    category: "blockchain",
+    image: Blockchain_outside5,
+},
+{
+    id: 51,
+    name: "Tiki hut",
+    category: "blockchain",
+    image: Blockchain_outside6,
+},
+{
+    id: 52,
+    name: "Inside Tiki hut ",
+    category: "blockchain",
+    image: Blockchain_outside7,
+},
+{
+    id: 53,
+    name: "Tikki hut back view ",
+    category: "blockchain",
+    image: Blockchain_outside8,
+},
+{
+    id: 54,
+    name: "Hot tub day time view ",
+    category: "blockchain",
+    image: Blockchain_outside9,
+},
+{
+    id: 55,
+    name: "Pool daytime view ",
+    category: "blockchain",
+    image: Blockchain_outside10,
+},
+{
+    id: 56,
+    name: "Daytime statue view",
+    category: "blockchain",
+    image: Blockchain_outside11,
+},
+{
+    id: 57,
+    name: "Tiki hut angle view",
+    category: "blockchain",
+    image: Blockchain_outside12,
+},
+{
+    id: 58,
+    name: "Hot tub side view ",
+    category: "blockchain",
+    image: Blockchain_outside13,
+},
+{
+    id: 59,
+    name: "Outside pool beach chair view ",
+    category: "blockchain",
+    image: Blockchain_outside14,
+},
+{
+    id: 60,
+    name: "Tiki hut with hot tub during the day ",
+    category: "blockchain",
+    image: Blockchain_outside15,
+},
+{
+    id: 61,
+    name: "Pool Outside Daytime view",
+    category: "blockchain",
+    image: Blockchain_outside20,
+},
+{
+    id: 62,
+    name: "Pool and chair Daytime side view ",
+    category: "blockchain",
+    image: Blockchain_outside21,
+},
+{
+    id: 63,
+    name: "Tiki hut tree view ",
+    category: "blockchain",
+    image: Blockchain_outside22 ,
+},
+{
+    id: 64,
+    name: "Tiki hut grass view",
+    category: "blockchain",
+    image: Blockchain_outside23,
+},
+{
+    id: 65,
+    name: "Tree house back view ",
+    category: "blockchain",
+    image: Blockchain_outside24,
+},
+{
+    id: 66,
+    name: "Tiki hut night view ",
+    category: "blockchain",
+    image: Blockchain_outside30,
+},
+{
+    id: 67,
+    name: "Tiki hut inside night view ",
+    category: "blockchain",
+    image: Blockchain_outside31,
+},
+{
+    id: 68,
+    name: "Tiki hut inside table view ",
+    category: "blockchain",
+    image: Blockchain_outside33,
+},
+{
+    id: 69,
+    name: "Night time house and tiki hut view ",
+    category: "blockchain",
+    image: Blockchain_outside34,
+},
+{
+    id: 70,
+    name: "Night time tiki hut view ",
+    category: "blockchain",
+    image: Blockchain_outside37,
+},
+{
+    id: 71,
+    name: "Night time patio view ",
+    category: "blockchain",
+    image: Blockchain_outside40,
+},
+{
+    id: 72,
+    name: "Daytime outside sky view ",
+    category: "blockchain",
+    image: Blockchain_outside50,
+},
+{
+    id: 73,
+    name: "Daytime outside sky view 2 ",
+    category: "blockchain",
+    image: Blockchain_outside51,
+},
+{
+    id: 74,
+    name: "Daytime outside sky view 3 ",
+    category: "blockchain",
+    image: Blockchain_outside58,
+},
+{
+    id: 75,
+    name: "Night pool with statue Gorilla ",
+    category: "blockchain",
+    image: Blockchain_pool,
+},
+{
+    id: 76,
+    name: "Open shower side view only ",
+    category: "blockchain",
+    image: Blockchain_shower1,
+},
+{
+    id: 77,
+    name: "Single sink view only ",
+    category: "blockchain",
+    image: Blockchain_sink1,
+},
+{
+    id: 78,
+    name: "Stair view only ",
+    category: "blockchain",
+    image: Blockchain_stairs1 ,
+},
+
+];
+
+export default all_product;
+
